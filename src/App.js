211b67edabbeaa4856e2ab96582a984e9037cca6
etherealfeel <graciagodsyndrome@gmail.com>
2023-05-05 21:36:46 +0300

    Move Apollo client and provider wrapper to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { useContext } from 'react';
 import { Navigation } from './components';
 import { CssBaseline, Container, Box } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { Home, Settings, Recommend } from './pages';
+import {
+  HttpLink,
+  ApolloLink,
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  from
+} from '@apollo/client';
+import { AppContext } from './context/appContext';
 
 function App() {
+  const { state } = useContext(AppContext);
+  const httpLink = new HttpLink({ uri: 'http://localhost:4000/' });
+  const localeMiddleware = new ApolloLink((operation, forward) => {
+    const customHeaders = operation.getContext().hasOwnProperty('headers')
+      ? operation.getContext().headers
+      : {};
+
+    operation.setContext({
+      headers: {
+        ...customHeaders,
+        locale: state.locale,
+      },
+    });
+    return forward(operation);
+  });
+  const client = new ApolloClient({
+    link: from([localeMiddleware, httpLink]),
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+  });
   return (
-    <>
+    <ApolloProvider client={client}>
       <CssBaseline />
       <Navigation />
       <Box
@@ -21,7 +51,7 @@ function App() {
           </Routes>
         </Container>
       </Box>
-    </>
+    </ApolloProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client';
+import { AppContextProvider } from './context/appContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache(),
-  connectToDevTools: true
-});
+
 root.render(
-  <ApolloProvider client={client}>
-    <Router>
+  <Router>
+    <AppContextProvider>
       <App />
-    </Router>
-  </ApolloProvider>,
+    </AppContextProvider>
+  </Router>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
